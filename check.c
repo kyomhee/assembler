87c8c9c3066bf4d8d1a7df8f0a47edd20438b916
kyomhee <sohee1303@naver.com>
2018-11-13 23:21:38 +0900

    my second commit

--- check.c
+++ check.c
@@ -4,12 +4,17 @@ int is_valid(char *op, char *args)
 {
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
 	
-	return 1;
+	int i;
+
+	for(i=0;i<strlen(args);i++)
+		if(args[i]==',') break;
+
+	if(args[0]=='0' || args[0]=='(' || args[0]=='-') {
+		if(args[i+1]=='0' || args[i+1]=='(' || args[i+1]=='-')
+			return 0;
+	}
+	else if(args[i+1]=='$')
+		 return 0;
+	else return 1;
 }
--- trans.c
+++ trans.c
@@ -8,14 +8,38 @@ int instr_trans(char *op, char *args, char* mcode)
 		return 0;
 	}
 
+	int i;
 
 	strcpy(mcode, "AB CD EF");
 
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
+	for(i=0; i<strlen(args);i++)
+		if(args[i]==',') break;
+
+	if(args[0]=='%' && args[i+1]=='%')
+		strcpy(mcode,"89");
+	if(args[0]=='(' || args[0]=='-')
+		if(args[i+1]=='%') strcpy(mcode,"8b");
+	if(args[0]=='$' && args[i+1]=='%') {
+		if(args[i+3]=='a') strcpy(mcode,"b8");
+		else if(args[i+3]=='c') strcpy(mcode,"b9");
+		else if(args[i+3]=='d') {
+			if(args[i+4]=='x')  strcpy(mcode,"ba");
+			else if(args[i+4]=='i') strcpy(mcode,"bf");
+		}	
+		else if(args[i+3]=='b') {
+			if(args[i+4]=='x') strcpy(mcode,"bb");
+			else if(args[i+4]=='p') strcpy(mcode,"bd");
+		}
+		else if(args[i+3]=='s') {
+			if(args[i+4]=='p') strcpy(mcode,"bc");
+			else if(args[i+4]=='i') strcpy(mcode,"be");
+		}
+	}
+	if(args[0]=='0' && args[i+1]=='%')
+		strcpy(mcode,"a1");
+	if(args[0]=='%') 
+		if(args[i+1]==')' || args[i+1]=='0' || args[i+1]=='-')
+			strcpy(mcode,"a3");
 	
 	return 1;	
 }
